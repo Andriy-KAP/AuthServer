34e419ff45aa4cc7db426a91493e4e8e4efa7c7f
akuklinov <akuklinov@bmfn.com>
2023-09-24 17:48:53 +0300

    Configuring AspNetCore.Identity

---
+++ AuthServer.Core/Core/AuthServerContext.cs
@@ -0,0 +1,18 @@
+using AuthServer.Domain.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Core
+{
+    public class AuthServerContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public AuthServerContext(DbContextOptions<AuthServerContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- AuthServer.Web/Program.cs
+++ AuthServer.Web/Program.cs
@@ -1,4 +1,10 @@
+using AuthServer.Core.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AuthServerContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
