72d85b527115e3cbc11575cb05b3e7cdde8c4905
akuklinov <akuklinov@bmfn.com>
2023-10-28 20:32:08 +0300

    Implementing role service.

---
+++ AuthServer.Core/Core/IRoleService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Core
+{
+    internal interface IRoleService
+    {
+        Task<IdentityResult> AddRole(string role);
+        Task<IdentityResult> RemoveRole(string id);
+        Task AddUserToRole(string userId, IEnumerable<string> rolesId);
+    }
+}
---
+++ AuthServer.Core/Services/RoleService.cs
@@ -0,0 +1,58 @@
+﻿using AuthServer.Core.Core;
+using AuthServer.Domain.Model;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    internal class RoleService : IRoleService
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RoleService(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task<IdentityResult> AddRole(string role)
+        {
+            if(string.IsNullOrEmpty(role))
+                throw new ArgumentNullException(nameof(role));
+            return await _roleManager.CreateAsync(new IdentityRole(role));
+        }
+
+        public async Task AddUserToRole(string userId, IEnumerable<string> rolesId)
+        {
+            ApplicationUser user = await _userManager.FindByIdAsync(userId);
+            if(user == null)
+            {
+                throw new NullReferenceException($"Cannot find user with id {userId}");
+            }
+            IList<string> userRoles = await _userManager.GetRolesAsync(user);
+            List<IdentityRole> allRoles = _roleManager.Roles.ToList();
+
+            List<string> addedRoles = rolesId.Except(userRoles).ToList<string>();
+            List<string> removedRoles = userRoles.Except(rolesId).ToList<string>();
+
+            await _userManager.AddToRolesAsync(user, addedRoles);
+            await _userManager.RemoveFromRolesAsync(user, removedRoles);
+
+        }
+
+        public async Task<IdentityResult> RemoveRole(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+            if(role == null)
+            {
+                throw new NullReferenceException("Cannot find role.");
+            }
+            return await _roleManager.DeleteAsync(role);
+        }
+    }
+}
